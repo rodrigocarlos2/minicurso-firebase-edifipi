14e08bc72ae0ec24c4381fd6b885c98018b87afc
kelvin <kelvsousa@gmail.com>
2016-10-24 20:22:15 -0300

    Projeto no inicio, nenhum passo implementado

---
+++ database/app.js
@@ -0,0 +1,75 @@
+
+function DatabaseAula(){
+
+	this.buttonPostar = document.getElementById("btnPostar");
+	this.entradaPostar = document.getElementById("entPostar");
+	this.postForm = document.getElementById("post-form")
+	this.postForm.addEventListener('submit', this.savePost.bind(this));
+	this.listPosts = document.getElementById("posts");
+
+	this.initFirebase();
+	
+}
+//Passo 1: Inicialiazação do Firebase
+DatabaseAula.prototype.initFirebase = function() {
+  //Inicialiazação dos serviços firebase
+  this.database = null; //TODO(DEVELOPER): Inicializar o serviço de database firebase.database()
+
+  //this.loadPosts(); //TODO(DEVELOPER): Descomentar esta linha depois de implementar a função loadPosts
+};
+
+//Passo 2: Carregar as mensagens do Firebase
+DatabaseAula.prototype.loadPosts = function() {
+  // Referencia aos Posts do Firebase
+  this.postsRef = null; //TODO(DEVELOPER): Criar uma nova referencia para /posts/
+  
+  // Só para garantir que não tem nenhum listener anterior
+  this.postsRef.off();
+  
+	var setPost = function(data) {
+    	var val = data.val();
+ 		this.displayMessage(data.key, val.name, val.text, val.photoUrl, val.imageUrl);
+	}.bind(this);
+  
+  this.postsRef.on('child_added', setPost);
+  this.postsRef.on('child_changed', setPost);
+};
+
+//Passo 3: Postar
+DatabaseAula.prototype.savePost = function(e) {
+  e.preventDefault();
+
+  if (this.entradaPostar.value) {
+    //this.postsRef.push
+
+	//TODO(DEVELOPER): implementar o push para enviar o post ao banco e depois apagar o campo de texto
+  }
+};
+
+
+//Função para criar um post 
+DatabaseAula.prototype.displayMessage = function(key, name, text, picUrl, imageUri) {
+  var div = document.getElementById(key);
+  // Se o elemento para o post não existir, cria um novo elemento
+  if (!div) {
+    var container = document.createElement('div');
+    container.innerHTML = DatabaseAula.POST_TEMPLATE;
+    div = container.firstChild;
+    div.setAttribute('id', key);
+    this.listPosts.insertBefore(div, this.listPosts.firstChild);
+  }
+  
+  div.querySelector('.text').textContent = text;
+  
+};
+
+DatabaseAula.POST_TEMPLATE =
+    '<div class="jumbotron spaces">'+
+	'<h1 class="text">Hello, world!</h1>'+
+	'<p><button class="btn btn-primary" type="button">  Curtidas <span class="badge">99</span> </button></p>'+
+	'</div>';
+
+
+window.onload = function() {
+  window.databaseAula = new DatabaseAula();
+};
